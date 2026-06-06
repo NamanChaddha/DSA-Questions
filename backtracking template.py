1cac5da434b1ee55c46dea0d92931146e2fd4c56
NamanChaddha <namanchaddha2501@gmail.com>
2026-06-06 13:01:57 +0530

    Add backtracking template structure

---
+++ backtracking template.py
@@ -0,0 +1,11 @@
+        def backtrack():
+            if len(nums)==:
+                result.append(copy of solution)
+                return
+            for choice in choices:
+                if violates constraints:
+                    continue
+
+                make choice
+                backtrack(update parameters)
+                undo choice
