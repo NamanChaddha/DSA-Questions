de273ad64f318a2733e29b290f3d9bdf1192e195
NamanChaddha <namanchaddha2501@gmail.com>
2026-06-07 12:00:52 +0530

    Implement countGoodNumbers method in Solution class

---
+++ goodnum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        MOD=10**9+7
+        s=0
+        if n%2==0:
+            s=(pow(5, n//2, MOD)*pow(4, n//2, MOD))
+        elif n%2!=0:
+            s= (pow(5,n//2+1,MOD)*pow(4, n//2, MOD))
+        return s%MOD
