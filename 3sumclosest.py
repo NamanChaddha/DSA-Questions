ba2999c86200bcff26c9c388bd145cbc68a00a81
NamanChaddha <namanchaddha2501@gmail.com>
2026-06-16 23:58:56 +0530

    Update 3sumclosest.py

--- 3sumclosest.py
+++ 3sumclosest.py
@@ -2,7 +2,7 @@ class Solution(object):
     def threeSumClosest(self, nums, target):
         nums.sort()
         cl=nums[0]+nums[1]+nums[2]
-        for i in range(len(nums)-2):
+        for i in range(len(nums)-2): 
             j=i+1
             k=len(nums)-1
             while j<k:
