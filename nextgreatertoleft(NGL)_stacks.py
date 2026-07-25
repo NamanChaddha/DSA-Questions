4f589916211b38a3e5360840c8fcf798d095b1e3
NamanChaddha <namanchaddha2501@gmail.com>
2026-07-25 17:05:05 +0530

    Add nextSmallerEle method using stacks in Solution class
    Implement nextSmallerEle method to find next smaller elements.

---
+++ nextgreatertoleft(NGL)_stacks.py
@@ -0,0 +1,20 @@
+class Solution:
+	def nextSmallerEle(self, arr):
+		# code here
+		st=[]
+		arr2=[]
+		for i in range(len(arr)-1,-1,-1):
+		    if st==[]:
+		        arr2.append(-1)
+		    elif arr[i]>st[-1]:
+		        arr2.append(st[-1])
+		    elif arr[i]<=st[-1]:
+		        while st!=[] and st[-1]>=arr[i]:
+		            st.pop()
+		        if st!=[]:
+		            arr2.append(st[-1])
+		        else:
+		            arr2.append(-1)
+		    st.append(arr[i])
+	    return arr2[::-1]
+		        
